fbfb9dbe629688678ec75f45f8aecd7c8f4c8073
V1n1c1us-K-S-W4t4n4b3 <vinikswatanabe@outlook.com>
2023-10-10 01:04:59 -0300

    Fisrt commit

---
+++ app/src/main/java/com/kzdev/first_generation_pokedexgit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kzdev.first_generation_pokedexgit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
