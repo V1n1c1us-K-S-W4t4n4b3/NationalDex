19b8c765f805d58db75388e893be4b89ce071bc5
V1n1c1us-K-S-W4t4n4b3 <vinikswatanabe@outlook.com>
2023-10-22 11:05:05 -0300

    add endpoint

--- app/src/main/java/com/kzdev/first_generation_pokedexgit/network/Endpoint.kt
+++ app/src/main/java/com/kzdev/first_generation_pokedexgit/network/Endpoint.kt
@@ -1,4 +1,12 @@
 package com.kzdev.first_generation_pokedexgit.network
 
-class Endpoint {
+import com.kzdev.first_generation_pokedexgit.model.Pokemon
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface Endpoint {
+
+    @GET("pokemon?limit=100000&offset=0")
+    fun getall(): Call<List<Pokemon>>
+
 }
