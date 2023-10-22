ad18ffad393c1c7ee4cb3b8b411d982ee3a6ab08
V1n1c1us-K-S-W4t4n4b3 <vinikswatanabe@outlook.com>
2023-10-22 19:51:00 -0300

    criação de adapter

---
+++ app/src/main/java/com/kzdev/first_generation_pokedexgit/UI/PokemonAdapter.kt
@@ -0,0 +1,51 @@
+package com.kzdev.first_generation_pokedexgit.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.kzdev.first_generation_pokedexgit.R
+import com.kzdev.first_generation_pokedexgit.model.Pokemon
+
+class PokemonAdapter(private val dataSet: List<Pokemon>) :
+    RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val id: TextView
+       // val ivPokemon: ImageView
+        val tvName: TextView
+        val tvType1: TextView
+        val tvType2: TextView
+
+        init {
+
+            id = view.findViewById(R.id.tv_numb_pokemon)
+           // ivPokemon = view.findViewById(R.id.pokemon_image)
+            tvName = view.findViewById(R.id.tv_name_pokemon)
+            tvType1 = view.findViewById(R.id.tv_type1)
+            tvType2 = view.findViewById(R.id.tv_type2)
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.pokemon_item, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        viewHolder.id.text = dataSet[position].id.toString()
+        viewHolder.tvName.text = dataSet[position].name
+        // viewHolder.ivPokemon.imageMatrix = dataSet[position].image.frontDefault
+        viewHolder.tvType1.text = dataSet[position].types.slot1.name
+        viewHolder.tvType2.text = dataSet[position].types.slot2.name
+    }
+
+    override fun getItemCount() = dataSet.size
+
+}
