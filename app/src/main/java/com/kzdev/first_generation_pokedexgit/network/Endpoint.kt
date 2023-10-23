c72cf25f67652bc765620d1f6151f9d58ad2575c
V1n1c1us-K-S-W4t4n4b3 <vinikswatanabe@outlook.com>
2023-10-22 23:14:59 -0300

    atualizaçao de network

--- app/src/main/java/com/kzdev/first_generation_pokedexgit/network/Endpoint.kt
+++ app/src/main/java/com/kzdev/first_generation_pokedexgit/network/Endpoint.kt
@@ -1,11 +1,12 @@
 package com.kzdev.first_generation_pokedexgit.network
 
 import com.kzdev.first_generation_pokedexgit.model.Pokemon
+import com.kzdev.first_generation_pokedexgit.modelrecyclerview.PokemonAll
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface EndPoint {
 
     @GET("pokemon?limit=100000&offset=0")
-    fun getAll(): Call<List<Pokemon>>
+    fun getAll(): Call<PokemonAll>
 }
