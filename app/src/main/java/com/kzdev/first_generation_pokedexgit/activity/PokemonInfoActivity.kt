dbd9c2775205e598e9f66fd090bf5555e0f795f6
V1n1c1us-K-S-W4t4n4b3 <vinikswatanabe@outlook.com>
2023-10-23 01:33:37 -0300

    att de config da activity pokemonInfo

--- app/src/main/java/com/kzdev/first_generation_pokedexgit/activity/PokemonInfoActivity.kt
+++ app/src/main/java/com/kzdev/first_generation_pokedexgit/activity/PokemonInfoActivity.kt
@@ -3,9 +3,10 @@ package com.kzdev.first_generation_pokedexgit.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import com.kzdev.first_generation_pokedexgit.databinding.ActivityPokemonInfoBinding
 import com.kzdev.first_generation_pokedexgit.model.Pokemon
-import com.kzdev.first_generation_pokedexgit.network.EndPontInfo
+import com.kzdev.first_generation_pokedexgit.network.EndPoint
 import com.kzdev.first_generation_pokedexgit.network.NetWorkUtils
 import retrofit2.Call
 import retrofit2.Callback
@@ -15,35 +16,32 @@ class PokemonInfoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPokemonInfoBinding
 
-    private lateinit var url : String
+    private lateinit var url: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPokemonInfoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        getData()
-
         receptor()
+
+        getData()
 
     }
 
 
-    private fun receptor(){
-
-        val name = intent.getStringExtra("name")
+    private fun receptor() {
         url = intent.getStringExtra("url").toString()
-
-        binding.textPokemon.text = name
 
     }
 
     private fun getData() {
 
-        val retrofitClient = NetWorkUtils.getRetrofitInstance(url)
+        //
+        val retrofitClient = NetWorkUtils.getRetrofitInstance("https://pokeapi.co/api/v2/")
 
-        val endPoint = retrofitClient.create(EndPontInfo::class.java)
-        val callback = endPoint.getPokemon()
+        val endPoint = retrofitClient.create(EndPoint::class.java)
+        val callback = endPoint.getPokemon(url)
 
         callback.enqueue(object : Callback<Pokemon> {
             override fun onFailure(call: Call<Pokemon>, t: Throwable) {
@@ -60,12 +58,27 @@ class PokemonInfoActivity : AppCompatActivity() {
                 // com o ?.let tenho certesa q passa uma lista msm q vazia
                 response.body()?.let {// qm chamar aqui?
 
+                    setUpView(it)
 
-                   // binding.textPokemon.text =
                 }
             }
         })
     }
 
+    fun setUpView(pokemon: Pokemon) {
 
+        binding.tvNamePokemon.text = pokemon.name
+        binding.tvNumbPokemon.text = pokemon.id.toString()
+
+        if (pokemon.types.size >= 1) {
+            binding.tvType1.text = pokemon.types[0].type.name
+            binding.tvType2.visibility = View.GONE
+        }
+
+        if (pokemon.types.size >= 2) {
+            binding.tvType2.text = pokemon.types[1].type.name
+            binding.tvType2.visibility = View.VISIBLE
+
+        }
+    }
 }
