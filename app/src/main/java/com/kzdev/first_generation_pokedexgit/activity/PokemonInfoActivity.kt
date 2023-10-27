c0fa2d2fb75daa9d3193c9f9d417aa9b9ce42abc
V1n1c1us-K-S-W4t4n4b3 <vinikswatanabe@outlook.com>
2023-10-27 01:47:44 -0300

    add troca de colors dos types de pokemon

--- app/src/main/java/com/kzdev/first_generation_pokedexgit/activity/PokemonInfoActivity.kt
+++ app/src/main/java/com/kzdev/first_generation_pokedexgit/activity/PokemonInfoActivity.kt
@@ -1,22 +1,29 @@
 package com.kzdev.first_generation_pokedexgit.activity
 
+import android.media.AudioMetadata
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import com.bumptech.glide.Glide
+import com.kzdev.first_generation_pokedexgit.R
 import com.kzdev.first_generation_pokedexgit.databinding.ActivityPokemonInfoBinding
 import com.kzdev.first_generation_pokedexgit.model.Pokemon
+import com.kzdev.first_generation_pokedexgit.model.PokemonType
 import com.kzdev.first_generation_pokedexgit.network.EndPoint
 import com.kzdev.first_generation_pokedexgit.network.NetWorkUtils
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.sql.Types
+import java.text.Format
 
 class PokemonInfoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPokemonInfoBinding
 
     private lateinit var url: String
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -65,18 +72,38 @@ class PokemonInfoActivity : AppCompatActivity() {
         })
     }
 
+    fun colorType(type: PokemonType): Int {
+
+        return when (type.name) {
+            "fire" -> getColor(R.color.red)
+            "water" -> getColor(R.color.blue)
+
+            else -> getColor(R.color.black)
+        }
+
+    }
+
     fun setUpView(pokemon: Pokemon) {
 
+
+        val format = String.format("#%03d", pokemon.id)
+
         binding.tvNamePokemon.text = pokemon.name
-        binding.tvNumbPokemon.text = pokemon.id.toString()
+        binding.tvNumbPokemon.text = format
+
+        Glide.with(this).load(pokemon.sprites.frontDefault).into(binding.pokemonImage)
+
 
         if (pokemon.types.size >= 1) {
             binding.tvType1.text = pokemon.types[0].type.name
+            binding.tvType1.setBackgroundColor(colorType(pokemon.types[0].type))
             binding.tvType2.visibility = View.GONE
+
         }
 
         if (pokemon.types.size >= 2) {
             binding.tvType2.text = pokemon.types[1].type.name
+            binding.tvType2.setBackgroundColor(colorType(pokemon.types[1].type))
             binding.tvType2.visibility = View.VISIBLE
 
         }
