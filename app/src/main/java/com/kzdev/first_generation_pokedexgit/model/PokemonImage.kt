23462c279f91db2d8c8b6e5f952cd9dd71cb20a0
V1n1c1us-K-S-W4t4n4b3 <vinikswatanabe@outlook.com>
2023-10-27 01:48:06 -0300

    serializando a imagem do pokemon

--- app/src/main/java/com/kzdev/first_generation_pokedexgit/model/PokemonImage.kt
+++ app/src/main/java/com/kzdev/first_generation_pokedexgit/model/PokemonImage.kt
@@ -1,7 +1,11 @@
 package com.kzdev.first_generation_pokedexgit.model
 
+import com.google.gson.annotations.SerializedName
+
 data class PokemonImage(
 
+
+    @SerializedName("front_default")
     val frontDefault: String
 
 )
