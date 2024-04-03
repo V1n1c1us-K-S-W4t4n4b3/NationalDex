70bb4b617cc7b4e4aa7ba98a7c1e43bc98ef6d2d
V1n1c1us-K-S-W4t4n4b3 <vinikswatanabe@outlook.com>
2024-04-03 17:34:38 -0400

    implementing search adapter

--- app/src/main/java/com/kzdev/first_generation_pokedexgit/activity/MainActivity.kt
+++ app/src/main/java/com/kzdev/first_generation_pokedexgit/activity/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.kzdev.first_generation_pokedexgit.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.kzdev.first_generation_pokedexgit.adapter.PokemonAdapter
+import com.kzdev.first_generation_pokedexgit.adapter.SearchAdapter
 import com.kzdev.first_generation_pokedexgit.databinding.ActivityMainBinding
 import com.kzdev.first_generation_pokedexgit.modelrecyclerview.PokeResults
 import com.kzdev.first_generation_pokedexgit.modelrecyclerview.PokemonAll
 import com.kzdev.first_generation_pokedexgit.network.EndPoint
 import com.kzdev.first_generation_pokedexgit.network.NetWorkUtils
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -19,62 +25,76 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var adapter: SearchAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         getData()
-
     }
 
     private fun openPokemonInfo(pokeResults: PokeResults) {
         val intent = Intent(this@MainActivity, PokemonInfoActivity::class.java)
-
         intent.putExtra("name", pokeResults.name)
         intent.putExtra("url", pokeResults.url)
-
         startActivity(intent)
     }
 
-    private fun setUpRecyclerView(dataSet: PokemonAll) {
-
+    private fun setUpPokeRecyclerView(dataSet: PokemonAll) {
         val rvPokemon = binding.rvPokemon
-
         rvPokemon.layoutManager = LinearLayoutManager(this)
         rvPokemon.adapter = PokemonAdapter(dataSet) {
-
             openPokemonInfo(it)
-
         }
+    }
 
+    private fun setUpSearchRecyclerView(dataSet: PokemonAll) {
+        val rvPokemon = binding.rvSearch
+        rvPokemon.layoutManager = LinearLayoutManager(this)
+
+        adapter = SearchAdapter(dataSet) { pokemon ->
+            openPokemonInfo(pokemon)
+        }
+        rvPokemon.adapter = adapter
+
+        binding.searchView.editText.addTextChangedListener {
+            lifecycleScope.launch {
+                findSupplierByText(it.toString())
+            }
+        }
+    }
+
+    private suspend fun findSupplierByText(text: String) {
+        binding.progress.visibility = View.VISIBLE
+        delay(500L)
+        adapter.filter.filter(text)
+        binding.progress.visibility = View.GONE
     }
 
     private fun getData() {
 
-            // declarar a url base
-            val retrofitClient = NetWorkUtils.getRetrofitInstance("https://pokeapi.co/api/v2/")
+        val retrofitClient = NetWorkUtils.getRetrofitInstance("https://pokeapi.co/api/v2/")
+        val endPoint = retrofitClient.create(EndPoint::class.java)
+        val callback = endPoint.getAll()
 
-            val endPoint = retrofitClient.create(EndPoint::class.java)
-            val callback = endPoint.getAll()
+        callback.enqueue(object : Callback<PokemonAll> {
+            override fun onFailure(call: Call<PokemonAll>, t: Throwable) {
+                t.printStackTrace()
+            }
 
-            callback.enqueue(object : Callback<PokemonAll> {
-                override fun onFailure(call: Call<PokemonAll>, t: Throwable) {
-                    //  Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
-                    t.printStackTrace()
+            override fun onResponse(
+                call: Call<PokemonAll>,
+                response: Response<PokemonAll>,
+            ) {
+                Log.i("Test", "ok")
+
+                response.body()?.let {
+                    setUpPokeRecyclerView(it)
+                    setUpSearchRecyclerView(it)
                 }
-
-                override fun onResponse(
-                    call: Call<PokemonAll>,
-                    response: Response<PokemonAll>,
-                ) {
-                    Log.i("Test", "ok")
-
-                    // com o ?.let tenho certesa q passa uma lista msm q vazia
-                    response.body()?.let {// qm chamar aqui?
-                        setUpRecyclerView(it)
-                    }
-                }
-            })
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/kzdev/first_generation_pokedexgit/adapter/SearchAdapter.kt
@@ -0,0 +1,61 @@
+package com.kzdev.first_generation_pokedexgit.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
+import androidx.recyclerview.widget.RecyclerView
+import com.kzdev.first_generation_pokedexgit.databinding.PokemonItemSearchBinding
+import com.kzdev.first_generation_pokedexgit.modelrecyclerview.PokeResults
+import com.kzdev.first_generation_pokedexgit.modelrecyclerview.PokemonAll
+
+class SearchAdapter (
+    private val dataSet: PokemonAll,
+    private val onItemClicked: (PokeResults) -> Unit,
+    ) : RecyclerView.Adapter<SearchAdapter.ViewHolder>(), Filterable {
+
+    private var filteredDataSet: MutableList<PokeResults> = mutableListOf()
+
+    inner class ViewHolder(val binding: PokemonItemSearchBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = PokemonItemSearchBinding.inflate(inflater, parent, false)
+            return ViewHolder(binding)
+        }
+
+        override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+            val name = dataSet.results[position].name
+            viewHolder.binding.tvNamePokemon.text = name
+            viewHolder.itemView.setOnClickListener {
+                onItemClicked(dataSet.results[position])
+            }
+        }
+
+        override fun getItemCount() = dataSet.results.size
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val filterPattern = constraint.toString().trim()
+
+                val filteredList = dataSet.results.filter {
+                    it.name.contains(filterPattern, true)
+                }
+
+                filteredDataSet.clear()
+                filteredDataSet.addAll(filteredList)
+
+                val filterResults = FilterResults()
+                filterResults.values = filteredDataSet
+
+                return filterResults
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                notifyDataSetChanged()
+            }
+        }
+    }
+}
